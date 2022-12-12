4d01d5cf7b1d7324f975c4f697e3befd9169dc4a
zaynaXs.com <amatullah11770@gmail.com>
2022-12-12 02:45:29 -0800

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ *main - print the alphabet in lowercase
+ *
+ *description: Will print out the entire alphabet in lowercase
+ *Return: 0
+ */
+int main(void)
+{
+	char a = 'z';
+
+	while (a <= 'a')
+	{
+		putchar(a);
+		a--;
+	}
+
+	putchar('\n');
+	return (0);
+}
