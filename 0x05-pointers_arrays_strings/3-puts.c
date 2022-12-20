493bdb4b0ce134830b3924e84bf7dc1cfc6c2007
zaynaXs.com <amatullah11770@gmail.com>
2022-12-20 08:10:18 -0800

    3. I do not fear computers. I fear the lack of them

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+int _strlen(char *s);
+
+/**
+ * _puts - put an array of characters
+ * @str: the string/array of char to put
+ */
+
+void _puts(char *str)
+{
+	int len;
+	int i;
+
+	len = _strlen(*str);
+	i = 0;
+	while (str[i] < len)
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar("\n");
+}
+
+/**
+ * _strlen - get length of a string
+ * @s: string to count
+ * Return: length of string as integer
+ */
+
+int _strlen(char *s)
+{
+	int l;
+
+	l = 0;
+	while (*str++)
+	{
+		l++;
+	}
+	return (l);
+}
+
