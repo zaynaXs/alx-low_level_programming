e0cb8436fa6073fa81049c71e902ac1930057667
zaynaXs.com <amatullah11770@gmail.com>
2022-12-20 10:28:22 -0800

    fix

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -13,11 +13,17 @@ void print_array(int *a, int n)
 	int i;
 
 	i = 0;
-	printf("%d", a[i]);
-	i++;
-	for ( ; i < n; i++)
+
+	for (i = 0; i < n; i++)
 	{
-		printf(", %d", a[i]);
+		if (i == 0)
+		{
+			printf("%d", a[i]);
+		}
+		else
+		{
+			printf(", %d", a[i]);
+		}
 	}
 	printf("\n");
 }
