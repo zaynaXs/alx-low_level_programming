fe473f01a101c72d45af07770dba990ca3723f45
zaynaXs.com <amatullah11770@gmail.com>
2022-12-20 09:25:18 -0800

    5-rev_string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,43 @@
+#include "main.h"
+int _strlen(char *s);
+
+/**
+ * rev_string - reverse a string
+ * @s: string to reverse
+ */
+
+void rev_string(char *s)
+{
+	int i;
+	int l;
+	int mid;
+	char temp;
+
+	l = _strlen(s);
+	mid = l / 2;
+	for (i = 0; i < mid; i++)
+	{
+		temp = s[i];
+		s[i] = s[l - i - 1];
+		s[l- i - 1] = temp;
+	}
+}
+
+/**
+ * _strlen - get length of a string
+ * @s: string to count
+ * Return: length of string as integer
+ */
+
+int _strlen(char *s)
+{
+	int l;
+
+	l = 0;
+	while (*s++)
+	{
+		l++;
+	}
+	return (l);
+}
+
